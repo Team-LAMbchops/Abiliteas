82753a3f1471763bbfa0656ba6f88c6a382cd4ab
Ming Chen <mzchen14@gmail.com>
2020-01-14 13:46:03 -0500

    feature: created Tea Model

---
+++ server/db/models/tea.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Tea = db.define('tea', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  flavor: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+      notEmpty: true
+    }
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    defaultValue: 100,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'http://cdn.shopify.com/s/files/1/0994/9584/products/sweet_tea_leaves-2_600x.jpg?v=1536186114',
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+})
+
+module.exports = Tea
