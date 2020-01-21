462ae0f4a7f9d6f6f58d7d0a7b1506c515d4bffe
Laura Altia Barger <laura.altia@gmail.com>
2020-01-21 13:22:37 -0500

    feature: new auth route for users to access their own data

--- server/api/securityMiddleware/check-Auth.js
+++ server/api/securityMiddleware/check-Auth.js
@@ -10,4 +10,16 @@ const isAdminMiddleware = (req, res, next) => {
   }
 }
 
-module.exports = {isAdminMiddleware}
+const isAuthMiddleware = (req, res, next) => {
+  try {
+    if (req.user.id === req.session.userId || req.user.isAdmin) {
+      next()
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Auth for this user failed'
+    })
+  }
+}
+
+module.exports = {isAdminMiddleware, isAuthMiddleware}
