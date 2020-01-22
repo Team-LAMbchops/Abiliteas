3f019e2159d82a47cb8c2c7e49a066b5ed840671
Ming Chen <mzchen14@gmail.com>
2020-01-22 12:31:20 -0500

    added css folder

---
+++ client/components/landingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+export const LandingPage = props => {
+  return (
+    <div>
+      <h1>Welcome to Abiliteas</h1>
+    </div>
+  )
+}
